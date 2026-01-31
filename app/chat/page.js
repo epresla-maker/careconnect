e92e0d5372967332269fdfae46d544d456f088a0
epresla-maker <epresla@icloud.com>
2026-01-31 20:57:52 +0100

    Remove group chat creation option from menu

--- app/chat/page.js
+++ app/chat/page.js
@@ -873,24 +873,6 @@ export default function ChatListPage() {
                 </svg>
               </button>
 
-              {/* Csoportos chat létrehozása */}
-              <button
-                onClick={() => {
-                  // TODO: Implementálni később
-                  alert('Csoportos chat funkció hamarosan!');
-                  setIsMenuOpen(false);
-                }}
-                className={`w-full flex items-center gap-4 p-4 text-left ${darkMode ? 'text-white bg-gray-800 hover:bg-gray-700' : 'text-gray-900 bg-gray-100 hover:bg-gray-200'} rounded-xl transition-colors`}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
-                </svg>
-                <span className="flex-1">Csoportos chat létrehozása</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 text-gray-400">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                </svg>
-              </button>
-
               {/*Archivált üzenetek */}
               <button
                 onClick={() => {
